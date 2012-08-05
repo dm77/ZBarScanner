e33f1867d03bd4eda8dbf26aa9429ce932465b49
Dushyanth Maguluru <dushyanth@zerista.com>
2012-08-05 04:03:13 -0600

    First commit.

---
+++ examples/ZBarScannerDemo/src/com/dm/zbar/android/examples/MainActivity.java
@@ -0,0 +1,58 @@
+package com.dm.zbar.android.examples;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.dm.zbar.android.scanner.ZBarConstants;
+import com.dm.zbar.android.scanner.ZBarScannerActivity;
+
+public class MainActivity extends Activity {
+
+    private static final int ZBAR_SCANNER_REQUEST = 0;
+    private static final int ZBAR_QR_SCANNER_REQUEST = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void launchScanner(View v) {
+        if (isCameraAvailable()) {
+            Intent intent = new Intent(this, ZBarScannerActivity.class);
+            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+        } else {
+            Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void launchQRScanner(View v) {
+        if (isCameraAvailable()) {
+            Intent intent = new Intent(this, ZBarScannerActivity.class);
+            intent.putExtra(ZBarScannerActivity.SCAN_MODES, new int[]{ZBarConstants.QRCODE});
+            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+        } else {
+            Toast.makeText(this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isCameraAvailable() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ZBAR_SCANNER_REQUEST:
+            case ZBAR_QR_SCANNER_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(this, "Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
---
+++ library/src/com/dm/zbar/android/scanner/ZBarScannerActivity.java
@@ -0,0 +1,109 @@
+package com.dm.zbar.android.scanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Window;
+import android.view.WindowManager;
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+
+public class ZBarScannerActivity extends Activity implements Camera.PreviewCallback, ZBarConstants {
+
+    private static final String TAG = "ZBarScannerActivity";        
+
+    private CameraPreview mPreview;
+    private Camera mCamera;
+    private ImageScanner mScanner;
+
+    static {
+        System.loadLibrary("iconv");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Create and configure the ImageScanner;
+        setupScanner();
+
+        // Create a RelativeLayout container that will hold a SurfaceView,
+        // and set it as the content of our activity.
+        mPreview = new CameraPreview(this, this);
+        setContentView(mPreview);
+    }
+
+    public void setupScanner() {
+        mScanner = new ImageScanner();
+        mScanner.setConfig(0, Config.X_DENSITY, 3);
+        mScanner.setConfig(0, Config.Y_DENSITY, 3);
+
+        int[] symbols = getIntent().getIntArrayExtra(SCAN_MODES);
+        if (symbols != null) {
+            mScanner.setConfig(Symbol.NONE, Config.ENABLE, 0);
+            for (int symbol : symbols) {
+                mScanner.setConfig(symbol, Config.ENABLE, 1);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Open the default i.e. the first rear facing camera.
+        mCamera = Camera.open();
+        mPreview.setCamera(mCamera);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Because the Camera object is a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        Camera.Parameters parameters = camera.getParameters();
+        Camera.Size size = parameters.getPreviewSize();
+
+        Image barcode = new Image(size.width, size.height, "Y800");
+        barcode.setData(data);
+
+        int result = mScanner.scanImage(barcode);
+
+        if (result != 0) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+
+            SymbolSet syms = mScanner.getResults();
+            for (Symbol sym : syms) {                
+                String symData = sym.getData();
+                if (!TextUtils.isEmpty(symData)) {
+                    Intent dataIntent = new Intent();
+                    dataIntent.putExtra(SCAN_RESULT, symData);
+                    dataIntent.putExtra(SCAN_RESULT_TYPE, sym.getType());
+                    setResult(Activity.RESULT_OK, dataIntent);
+                    finish();
+                    break;
+                }
+            }
+        }
+    }
+}
